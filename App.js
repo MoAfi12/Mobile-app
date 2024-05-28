c2d3234ebde62be4f9cbf8b1aeab09fba2599825
MoAfi12 <maxamedcaafi60@gmail.com>
2024-05-28 21:22:30 +0300

    Mobile app building react native

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Image style={{width:200 , height:200}} source={{uri:"https://img.freepik.com/free-photo/view-spectacular-nature-landscape_23-2150763636.jpg"}}/>
+      <Text>hello daer</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,7 +14,7 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1, 
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
